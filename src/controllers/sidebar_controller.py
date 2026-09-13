8a518a16385b4a2fc3257a12672e670b95f177fd
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-09-13 15:44:53 -0400

    se implemento la fucncio del archivo funciones del sistema para cambiar de pantalla

--- src/controllers/sidebar_controller.py
+++ src/controllers/sidebar_controller.py
@@ -1,6 +1,7 @@
 from views import SideBarView
 from controllers.pantalla_form_controller import PantallaFormController
 from controllers.pantalla_de_bienvenida_controller import PantallaDeBienvenidaController
+from utils import funciones_sistema
 
 
 class SideBarController(SideBarView):
@@ -8,14 +9,16 @@ class SideBarController(SideBarView):
         super().__init__()
         self.contenedor_principal = contenedor_principal
 
-        self.btn_principal.on_click = lambda: self.cambiar(PantallaDeBienvenidaController())
-        self.btn_salir.on_click = lambda: self.cambiar(PantallaFormController())
+        self.btn_principal.on_click = lambda e: funciones_sistema.cambiar_pantalla("PantallaDeBienvenida", self.contenedor_principal, e)
+        self.btn_form.on_click = lambda e: funciones_sistema.cambiar_pantalla("PantallaForm", self.contenedor_principal, e)
+        self.btn_salir.on_click = lambda e: self.volver_al_login(e)
 
-    def cambiar(self, contenido):
-        self.contenedor_principal.content = contenido
+    def volver_al_login(self, e):
+        funciones_sistema.cambiar_pantalla("Login", self.contenedor_principal, e)
+        self.visible = False
+        e.page.update()
 
-
-
+    
     
 
 

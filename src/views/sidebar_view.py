508f405c3dd437f529fd6df084d61c62c87de4f3
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-09-13 15:46:06 -0400

    se ajusto el estado inicial del sidebar

--- src/views/sidebar_view.py
+++ src/views/sidebar_view.py
@@ -5,10 +5,11 @@ class SideBarView(ft.Container):
         super().__init__()  
         
         self.bgcolor= ft.Colors.WHITE_10
-        self.width=200
+        self.width=50
         self.padding = 2
         self.animate = ft.Animation(200, curve= ft.AnimationCurve.LINEAR)
-        self.btn_menu = ft.Button("Menu", 
+        self.btn_menu = ft.Button(
+                                  content= "", 
                                   on_click= self.animarsidebar,
                                   icon = ft.Icons.MENU,
                                     expand= True,
@@ -16,21 +17,30 @@ class SideBarView(ft.Container):
                                     )
 
         self.btn_principal = ft.Button(
-                                       content= "Principal",
+                                       content= "",
                                        icon = ft.Icons.HOME,
                                        expand= True
                                        )
-        
+
+
+        self.btn_form = ft.Button(
+                            content= "",
+                            icon= ft.Icons.FOUR_MP,
+                            expand= True
+                            )
+
         self.btn_salir = ft.Button(
-                                   content= "Salir",
-                                   icon= ft.Icons.DOOR_BACK_DOOR,
-                                       expand= True
+                                   content= "",
+                                   icon= ft.Icons.ARROW_BACK,
+                                    expand= True
                                    )
         
         self.content= ft.Column(controls = [
                                         ft.Row(self.btn_menu),
                                         ft.Row(self.btn_principal),
+                                        ft.Row(self.btn_form),
                                         ft.Row(self.btn_salir)
+                                        
                                         
                                     ]
                             )
@@ -41,11 +51,12 @@ class SideBarView(ft.Container):
             self.width = 50
             self.btn_menu.content = ""
             self.btn_principal.content = ""
+            self.btn_form.content = ""
             self.btn_salir.content = ""
             
         elif self.width == 50:
             self.width = 200
-            time.sleep(0.2)
             self.btn_menu.content = "Menu"
             self.btn_principal.content = "Principal"
+            self.btn_form.content = "Form"
             self.btn_salir.content = "Salir"

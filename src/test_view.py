e90749c0bfd698dcea9327b15757e0da2bac7823
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-09-14 19:19:34 -0400

    archivo para ejecutar una view para ver el proceso de desarollo

---
+++ src/test_view.py
@@ -0,0 +1,32 @@
+
+import flet as ft
+from views.pantalla_form_view import PantallaFormView
+
+def main(page: ft.Page):
+    page.padding = 0
+    #page.width = 300
+    #page.theme_mode = ft.ThemeMode.LIGHT
+    controlTest = PantallaFormView()
+
+    print(f"Ancho inicial: {page.width}px")
+
+    # Función que se ejecuta al cambiar el tamaño de la ventana
+    def al_redimensionar(e):
+        print(f"Ancho actual: {page.width}px")
+
+    # Escucha el evento de redimensionado
+    page.on_resized = lambda e: al_redimensionar(e)
+    
+ 
+    page.add(
+        ft.Column(
+            controls=[controlTest],
+            expand= True
+            
+        )
+    )
+
+   
+
+if __name__ == "__main__":
+    ft.run(main)

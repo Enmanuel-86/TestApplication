3cab022601cd5d36506daef120219cebbdd5df0f
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-09-15 20:47:58 -0400

    que se ejecute mientras tanto como app de escritorio

--- src/main.py
+++ src/main.py
@@ -35,11 +35,11 @@ def main(page: ft.Page):
 
 if __name__ == "__main__":
 
-    #ft.run(main)
+    ft.run(main)
 
-    #"""
+    """
     ft.run(main,
            host = "0.0.0.0",
            port = 8080 ,
            view= ft.AppView.WEB_BROWSER)
-    #"""
+    """

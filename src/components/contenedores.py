df86b720259f3e963844f648d1ad3d8ef1e97c52
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-09-15 20:32:11 -0400

    se agrego un date picker con un label ya que flet no trae uno mas facil de usar

--- src/components/contenedores.py
+++ src/components/contenedores.py
@@ -1,4 +1,5 @@
 import flet as ft
+import datetime
 
 # Contenedor base
 @ft.control
@@ -13,4 +14,43 @@ class ContenedorBase(ft.Container):
 @ft.control
 class CardContainer(ContenedorBase):
     def build(self):
+        return super().build()
+
+@ft.control
+class DatePicker(ft.Container):
+    def __init__(self, titulo:str):
+        super().__init__()
+
+        self.titulo = titulo
+        
+
+    def build(self):
+        self.col={"xs": 12, "md": 3, "lg":3}
+
+        fecha_defecto = datetime.datetime.now().strftime("%d/%m/%Y")
+        today = datetime.datetime.now()
+        self.lbl_fecha = ft.Text(value = fecha_defecto, tooltip= "Dia/Mes/Año")
+
+        def handle_change(e: ft.Event[ft.DatePicker]):
+            self.lbl_fecha.value = e.control.value.strftime('%d/%m/%Y')
+
+        picker = ft.DatePicker(
+                                first_date=datetime.datetime(year=1964, month=1, day=1),
+                                last_date=datetime.datetime(year=today.year + 1, month=today.month, day=20),
+                                current_date=today,
+                                on_change=handle_change
+                                )
+
+        self.content = ft.Row(
+                              controls=[
+                                        ft.Button(
+                                                  icon=ft.Icons.CALENDAR_MONTH,
+                                                  on_click=lambda e: e.page.show_dialog(picker),
+                                                  content= self.titulo,
+                                                  tooltip= f"Pica el boton para seleccionar la {self.titulo.lower()}"
+                                                  ),
+                                        self.lbl_fecha
+                                                  ]
+                                                  )
+
         return super().build()

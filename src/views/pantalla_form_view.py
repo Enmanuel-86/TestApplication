94239f1eeadf740eebc01dbcd8568ebacffc1aae
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-09-15 20:48:58 -0400

    se agrego un dropdown para seleccionar el genero de la persona y date picker que se hizo a mano ya que flet no trae uno mas simple

--- src/views/pantalla_form_view.py
+++ src/views/pantalla_form_view.py
@@ -1,5 +1,6 @@
 import flet as ft
 from components import CardContainer, TextFieldForm
+from components.contenedores import DatePicker
 
 class PantallaFormView(ft.Container):
     def __init__(self):
@@ -30,6 +31,20 @@ class PantallaFormView(ft.Container):
         self.txt_primero_apellido = TextFieldForm(label= "Primer Apellido")
         self.txt_segundo_apellido = TextFieldForm(label= "Segundo Apellido")
         self.txt_tercer_apellido = TextFieldForm(label= "Tercer Apellido")
+        self.txt_cedula = TextFieldForm(label="Cédula")
+
+        self.dd_genero = ft.DropdownM2(label= "Genero",
+                                     col={"xs": 12, "md": 3, "lg":3},
+                                     expand= True,
+                                     options= [
+                                         ft.DropdownOption("Masculino"),
+                                         ft.DropdownOption("Femenino")
+                                     ]
+                                        )
+        
+        self.dp_fecha_de_nacimiento = ft.DatePicker(
+                                                    
+                                                    )
 
         self.cnt_form_datos_personales = CardContainer(
                                                        content= ft.ResponsiveRow(
@@ -40,7 +55,11 @@ class PantallaFormView(ft.Container):
                                                                                  self.txt_segundo_nombre,
                                                                                  self.txt_primero_apellido,
                                                                                  self.txt_segundo_apellido,
-                                                                                 self.txt_tercer_apellido
+                                                                                 self.txt_tercer_apellido,
+                                                                                 self.dd_genero,
+                                                                                 self.txt_cedula,
+                                                                                 DatePicker("Fecha nacimiento")
+                                                                                 
                                                                                  ]
                                                                        )
                                                        )

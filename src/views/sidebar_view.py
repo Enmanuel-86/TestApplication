d2cbe03fbae1986b70e449c43aafeca6bb9c170a
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-09-24 20:31:00 -0400

    se implemento el boton personalizado

--- src/views/sidebar_view.py
+++ src/views/sidebar_view.py
@@ -1,57 +1,40 @@
 import flet as ft
 import time
+from components import ButtonSideBar
 class SideBarView(ft.Container):
     def __init__(self):
         super().__init__()  
         
         self.bgcolor= ft.Colors.WHITE_10
-        self.width=50
+        self.width=47
         self.padding = 2
         self.animate = ft.Animation(200, curve= ft.AnimationCurve.LINEAR)
-        self.btn_menu = ft.Button(
-                                  content= ft.Text("Menu", no_wrap= True), 
+        self.btn_menu = ButtonSideBar(
+                                  titulo= "Menu",
+                                  icono = ft.Icons.MENU, 
                                   on_click= self.animarsidebar,
-                                  icon = ft.Icons.MENU,
-                                    expand= True,
-                                    
-                                    )
+                                  )
 
-        self.btn_principal = ft.Button(
-                                       content= ft.Text("Principal", no_wrap= True),
-                                       icon = ft.Icons.HOME,
-                                       expand= True
-                                       )
+        self.btn_principal = ButtonSideBar(titulo= "Principal", icono= ft.Icons.HOME)
 
 
-        self.btn_form = ft.Button(
-                            content= ft.Text("Formulario", no_wrap= True),
-                            icon= ft.Icons.FOUR_MP,
-                            expand= True
-                            )
+        self.btn_form = ButtonSideBar(titulo = "Formulario", icono= ft.Icons.NOTE_ADD)
 
-        self.btn_salir = ft.Button(
-                                   content= ft.Text("Salir", no_wrap= True),
-                                   icon= ft.Icons.ARROW_BACK,
-                                    expand= True,
-                                    
-                                    
-                                   )
+        self.btn_salir = ButtonSideBar(titulo= "Salir", icono= ft.Icons.ARROW_BACK )
         
         self.content= ft.Column(controls = [
                                         ft.Row(self.btn_menu),
                                         ft.Row(self.btn_principal),
                                         ft.Row(self.btn_form),
-                                        ft.Row([self.btn_salir])
-                                        
-                                        
-                                    ]
+                                        ft.Row([self.btn_salir])],
+                                spacing= 2
                             )
 
 
     def animarsidebar(self):
         if self.width == 200:
-            self.width = 50
+            self.width = 47
 
-        elif self.width == 50:
+        elif self.width == 47:
             self.width = 200
             
